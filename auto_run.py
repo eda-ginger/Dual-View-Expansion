900823e42f722845d890954de6bdafbee95bb8c3
park <rudwls2717@pusan.ac.kr>
2024-10-13 22:07:02 +0900

    init

--- auto_run.py
+++ auto_run.py
@@ -15,18 +15,18 @@ def split(s):
     return params
 
 
-# cmd_lst = ['--data ./TDC/DTA/DAVIS/random --d1_col Drug --d2_col Target_ID --d1_type Drug --d2_type Protein --data_name DAVIS --task_name DTA --project_name DAVIS_random_DSN --n_workers 10 --n_epochs 200 --lr 1e-3 --batch_size 1024 --architecture DSN',
-#            '--data ./TDC/DTA/DAVIS/cold_split/Drug --d1_col Drug --d2_col Target_ID --d1_type Drug --d2_type Protein --data_name DAVIS --task_name DTA --project_name DAVIS_cold_drug_DSN --n_workers 10 --n_epochs 200 --lr 1e-3 --batch_size 1024 --architecture DSN',
-#            '--data ./TDC/DTA/DAVIS/cold_split/Target --d1_col Drug --d2_col Target_ID --d1_type Drug --d2_type Protein --data_name DAVIS --task_name DTA --project_name DAVIS_cold_target_DSN --n_workers 10 --n_epochs 200 --lr 1e-3 --batch_size 1024 --architecture DSN',
-#            '--data ./TDC/DTA/KIBA/random --d1_col Drug --d2_col Target_ID --d1_type Drug --d2_type Protein --data_name KIBA --task_name DTA --project_name KIBA_random_DSN --n_workers 10 --n_epochs 200 --lr 1e-3 --batch_size 1024 --architecture DSN',
-#            '--data ./TDC/DTA/KIBA/cold_split/Drug --d1_col Drug --d2_col Target_ID --d1_type Drug --d2_type Protein --data_name KIBA --task_name DTA --project_name KIBA_cold_drug_DSN --n_workers 10 --n_epochs 200 --lr 1e-3 --batch_size 1024 --architecture DSN',
-#            '--data ./TDC/DTA/KIBA/cold_split/Target --d1_col Drug --d2_col Target_ID --d1_type Drug --d2_type Protein --data_name KIBA --task_name DTA --project_name KIBA_cold_target_DSN --n_workers 10 --n_epochs 200 --lr 1e-3 --batch_size 1024 --architecture DSN',
-#            ]
-
-cmd_lst = ['--data ./TDC/DTA/DAVIS/random --d1_col Drug --d2_col Target_ID --d1_type Drug --d2_type Protein --data_name DAVIS --task_name DTA --project_name DAVIS_random_DSN --n_workers 4 --n_epochs 200 --lr 1e-3 --batch_size 512 --architecture DSN',
-           '--data ./TDC/DTA/DAVIS/cold_split/Drug --d1_col Drug --d2_col Target_ID --d1_type Drug --d2_type Protein --data_name DAVIS --task_name DTA --project_name DAVIS_cold_drug_DSN --n_workers 4 --n_epochs 200 --lr 1e-3 --batch_size 512 --architecture DSN',
-           '--data ./TDC/DTA/DAVIS/cold_split/Target --d1_col Drug --d2_col Target_ID --d1_type Drug --d2_type Protein --data_name DAVIS --task_name DTA --project_name DAVIS_cold_target_DSN --n_workers 4 --n_epochs 200 --lr 1e-3 --batch_size 512 --architecture DSN'
+cmd_lst = ['--data ./TDC/DTA/DAVIS/random --d1_col Drug --d2_col Target_ID --d1_type Drug --d2_type Protein --data_name DAVIS --task_name DTA --project_name DAVIS_random_DSN --n_workers 10 --n_epochs 200 --lr 1e-3 --batch_size 1024 --architecture DSN',
+           '--data ./TDC/DTA/DAVIS/cold_split/Drug --d1_col Drug --d2_col Target_ID --d1_type Drug --d2_type Protein --data_name DAVIS --task_name DTA --project_name DAVIS_cold_drug_DSN --n_workers 10 --n_epochs 200 --lr 1e-3 --batch_size 1024 --architecture DSN',
+           '--data ./TDC/DTA/DAVIS/cold_split/Target --d1_col Drug --d2_col Target_ID --d1_type Drug --d2_type Protein --data_name DAVIS --task_name DTA --project_name DAVIS_cold_target_DSN --n_workers 10 --n_epochs 200 --lr 1e-3 --batch_size 1024 --architecture DSN',
+           '--data ./TDC/DTA/KIBA/random --d1_col Drug --d2_col Target_ID --d1_type Drug --d2_type Protein --data_name KIBA --task_name DTA --project_name KIBA_random_DSN --n_workers 10 --n_epochs 200 --lr 1e-3 --batch_size 1024 --architecture DSN',
+           '--data ./TDC/DTA/KIBA/cold_split/Drug --d1_col Drug --d2_col Target_ID --d1_type Drug --d2_type Protein --data_name KIBA --task_name DTA --project_name KIBA_cold_drug_DSN --n_workers 10 --n_epochs 200 --lr 1e-3 --batch_size 1024 --architecture DSN',
+           '--data ./TDC/DTA/KIBA/cold_split/Target --d1_col Drug --d2_col Target_ID --d1_type Drug --d2_type Protein --data_name KIBA --task_name DTA --project_name KIBA_cold_target_DSN --n_workers 10 --n_epochs 200 --lr 1e-3 --batch_size 1024 --architecture DSN'
            ]
+
+# cmd_lst = ['--data ./TDC/DTA/DAVIS/random --d1_col Drug --d2_col Target_ID --d1_type Drug --d2_type Protein --data_name DAVIS --task_name DTA --project_name DAVIS_random_DSN --n_workers 4 --n_epochs 200 --lr 1e-3 --batch_size 512 --architecture DSN',
+#            '--data ./TDC/DTA/DAVIS/cold_split/Drug --d1_col Drug --d2_col Target_ID --d1_type Drug --d2_type Protein --data_name DAVIS --task_name DTA --project_name DAVIS_cold_drug_DSN --n_workers 4 --n_epochs 200 --lr 1e-3 --batch_size 512 --architecture DSN',
+#            '--data ./TDC/DTA/DAVIS/cold_split/Target --d1_col Drug --d2_col Target_ID --d1_type Drug --d2_type Protein --data_name DAVIS --task_name DTA --project_name DAVIS_cold_target_DSN --n_workers 4 --n_epochs 200 --lr 1e-3 --batch_size 512 --architecture DSN'
+#            ]
 
 import time
 for command in cmd_lst:
